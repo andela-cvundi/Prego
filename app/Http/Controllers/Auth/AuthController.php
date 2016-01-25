e7e4bcd0303f1b2d00bab687797ee8f57fd5197f
andela-cvundi <christophervundi@gmail.com>
2016-01-25 21:53:43 +0300

    Changed namespace to prego

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Prego\Http\Controllers\Auth;
 
-use App\User;
+use Prego\User;
 use Validator;
-use App\Http\Controllers\Controller;
+use Prego\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Prego\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Prego\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller
