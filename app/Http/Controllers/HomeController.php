8efd93ce6ca4ea65e65bab29d9e25db6cd1d637f
andela-cvundi <christophervundi@gmail.com>
2016-01-25 22:24:02 +0300

    Changed homepage to my own

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Prego\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Prego\Http\Requests;
+use Prego\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+}
